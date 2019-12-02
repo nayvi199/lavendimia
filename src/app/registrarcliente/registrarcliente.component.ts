8db0b13b1d7e731a72dbfc948017b722ce952bf1
nayvi199 <nayvinight12@gmail.com>
2019-12-01 22:18:05 -0700

    rfc val
    validaciones rfc val

--- src/app/registrarcliente/registrarcliente.component.ts
+++ src/app/registrarcliente/registrarcliente.component.ts
@@ -5,6 +5,7 @@ import { ToasterService } from 'angular2-toaster';
 import { ICliente } from '../models/cliente';
 import { ServiceLService } from '../services/service-l.service';
 import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-registrarcliente',
@@ -30,7 +31,7 @@ export class RegistrarclienteComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.inicializarForm();
-
+    this.generarFechaAleatoria();
     this.sub = this.route.params.subscribe((params) => {
       this.id = params.id;
       console.log('id:' + this.id);
@@ -65,7 +66,7 @@ export class RegistrarclienteComponent implements OnInit, OnDestroy {
         nom_cliente : new FormControl('', Validators.required ),
         ape_paterno : new FormControl('', Validators.required),
         ape_materno : new FormControl('', Validators.required),
-        rfc : new FormControl('', Validators.required),
+        rfc : new FormControl({value: '', disabled: true}, Validators.required),
       }
   );
   }
@@ -129,11 +130,72 @@ export class RegistrarclienteComponent implements OnInit, OnDestroy {
     });*/
   }
 
-  myFunction() {
-    console.log('entro a myfuncion');
+  /*Forma de Llamarse
+  <input type="text" formControlName="ape_paterno" class="form-control" name="ape_paterno" 
+  placeholder="Ingrese Apellido Paterno:" (ngModelChange)="onChangeServicio($event)"/> 
+  */
+
+  onChangeServicio(event) {
+    console.log('entro a myfuncion' + event);
   }
 
+  autoCompletarRFC() {
+    const nom = (this.clientesForm.getRawValue().nom_cliente).toUpperCase();
+    const apeP = (this.clientesForm.getRawValue().ape_paterno).toUpperCase();
+    const apeM = (this.clientesForm.getRawValue().ape_materno).toUpperCase();
+    const arr = [];
+    if (nom != null && apeP != null && apeM) {
+       // Buscar primero vocal en apellido
+       let i1 = (apeP.indexOf('A'));
+       let i2 = (apeP.indexOf('E'));
+       let i3 = (apeP.indexOf('I'));
+       let i4 = (apeP.indexOf('O'));
+       let i5 = (apeP.indexOf('U'));
+       /*console.log('i1: ' + i1);
+       console.log('i2: ' + i2);
+       console.log('i3: ' + i3);
+       console.log('i5: ' + i4);
+       console.log('i5: ' + i5);*/
+       if (i1 == -1 || i1 == 0) {
+        i1 = 20;
+       }
+       if (i2 == -1 || i2 == 0) {
+        i2 = 20;
+       }
+       if (i3 == -1 || i3 == 0) {
+        i3 = 20;
+       }
+       if (i4 == -1 || i4 == 0) {
+        i4 = 20;
+       }
+       if (i5 == -1 || i5 == 0) {
+        i5 = 20;
+       }
+       let indice = Math.min(i1, i2, i3, i4, i5);
+       if ( indice == -1) {
+           indice = 1;
+       }
+       this.rfcAuto = ((apeP.charAt(0) + apeP.charAt(indice)) + (apeM.charAt(0)) + (nom.charAt(0)) );
+       this.clientesForm.controls.rfc.setValue(this.rfcAuto);
+    }
+  }
 
+  generarFechaAleatoria() {
+      let anio;
+      let mes;
+      let mes1;
+      let dia;
+      let dia2;
+      anio = Math.round(Math.random() * (2019 - 1980) + 1990);
+      mes = Math.round(Math.random() * (12 - 0o1) + 0o1);
+      dia = Math.round(Math.random() * (30 - 0o1) + 0o1);
+      console.log('anio' + anio);
+      console.log('mes' + mes);
+      console.log('dia' + dia);
+      if (dia.lenght == 0 ) {
+        
+      }
+  }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
